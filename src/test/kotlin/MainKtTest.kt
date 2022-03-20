a668348b6340f142c0c43a6ef05bccfa04744404
Denisshef <denisshef24@yandex.ru>
2022-03-20 10:47:00 +0700

    Create file testing 'MainKtTest.kt'

---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,21 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainKtTest {
+
+    @Test
+    fun test1() {
+        // arrange
+        val value = 5
+
+        // act
+        val result = test(
+            i = value
+        )
+
+        // assert
+        assertEquals(15, result)
+
+    }
+}
